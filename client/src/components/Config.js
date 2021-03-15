4c3b44f0f53a01ad8c98ca083adca21574f78e24
jooheek <kimjoohee713@gmail.com>
2021-03-15 17:54:49 +0900

    Create Load More movies function

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,11 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+//API URL 을 EXPORT 해서 중복된 코드를 줄임
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+//IMG URL 을 EXPORT해서 중복된 이미지 코드를 줄임
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+
+//API_KEY정의
+export const API_KEY = '18ed98604f28635109bb0175e23b4d10';
